04d438d6b703874fdf5887d0a2e0795940c48569
Oles <bolotniuk.oles@gmail.com>
2017-12-02 03:17:36 +0200

    changed stats component order

--- src/components/StatsComponent.js
+++ src/components/StatsComponent.js
@@ -96,6 +96,14 @@ export default class StatsComponent extends React.Component {
 
                 
                     <div class='group'>
+
+                        <div class='item'>
+                            <div class='name' title={chrome.i18n.getMessage('stats_all_users')}>
+                                {chrome.i18n.getMessage('stats_total_users')}:
+                            </div>
+                            <div class='value'>{ this.props.stats.totalUsers }</div>
+                        </div>
+
                         <div class='item'>
                             <div class='name' title={chrome.i18n.getMessage('stats_all_users')}>
                                 {chrome.i18n.getMessage('stats_total_visited')}:
@@ -115,13 +123,6 @@ export default class StatsComponent extends React.Component {
                                 {chrome.i18n.getMessage('stats_total_bookmarks')}:
                             </div>
                             <div class='value'>{ this.props.stats.totalBookmarks }</div>
-                        </div>
-
-                        <div class='item'>
-                            <div class='name' title={chrome.i18n.getMessage('stats_all_users')}>
-                                {chrome.i18n.getMessage('stats_total_users')}:
-                            </div>
-                            <div class='value'>{ this.props.stats.totalUsers }</div>
                         </div>
 
                         <div class='item'>
